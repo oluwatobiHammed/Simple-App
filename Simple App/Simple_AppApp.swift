a5ee1fd79f8c5fcf450f89ff83e3a480fe17a720
Oluwa-ios <oladipupo_tobi@yahoo.com>
2025-08-11 15:02:13 -0400

    Initial Commit

---
+++ Simple App/Simple_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_AppApp.swift
+//  Simple App
+//
+//  Created by Oluwatobi Oladipupo on 2025-08-11.
+//
+
+import SwiftUI
+
+@main
+struct Simple_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple AppTests/Simple_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Simple_AppTests.swift
+//  Simple AppTests
+//
+//  Created by Oluwatobi Oladipupo on 2025-08-11.
+//
+
+import Testing
+
+struct Simple_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
